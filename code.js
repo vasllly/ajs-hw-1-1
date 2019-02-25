552e2eca40477991d6981730a102472cb32a779c
Ganja Vasiliy <vasllly219@gmail.com>
2019-02-25 16:18:34 +0300

    Done!

---
+++ code.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const goods_prices = [200, 550, 4000, 23, 58, 5000, 485, 711];
+
+const calculateBonus = (prices, minimum_sum = 10000, discount_in_percent = 5) => {
+  let sum = sumArray(prices)
+  if (sum > minimum_sum) {
+    return Math.trunc(sum * discount_in_percent / 100);
+  } else {
+    return 0;
+  }
+}
+
+const  sumArray = (array) => {
+  let sum = 0;
+  for (let value of array) {
+    sum += value;
+  }
+  return sum;
+}
+
+calculateBonus(goods_prices);
